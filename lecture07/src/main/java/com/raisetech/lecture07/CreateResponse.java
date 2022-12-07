3e4823b61d5f502e24cfbbed8bdde2022d0702c0
kazuoikeda1411 <erizabesu1411@icloud.com>
2022-12-07 19:18:31 +0900

    コメント対応（レスポンスの形式の見直し、ハードコード削除、クエリ名user部分を削除

---
+++ lecture07/src/main/java/com/raisetech/lecture07/CreateResponse.java
@@ -0,0 +1,19 @@
+package com.raisetech.lecture07;
+
+public class CreateResponse {
+	private int id;
+	private String message;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
--- lecture07/src/main/java/com/raisetech/lecture07/HelloController.java
+++ lecture07/src/main/java/com/raisetech/lecture07/HelloController.java
@@ -17,31 +17,31 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class HelloController {
 
-	private static final String template = " 登録氏名：%s 登録生年月日：%s user id：%d";
+	private static final String template = " 登録氏名：%s 登録生年月日：%s id：%d";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/users")
 	public String greeting(
-		@RequestParam @Size(max=20) @NotBlank String userName, @RequestParam("userDateOfBirth") @Pattern(regexp="^([MTSH]\\d{1,2}|\\d{2,4})/?(0?[1-9]|1[0-2])/?(0?[1-9]|[1-2][0-9]|3[0-1])$") @NotBlank String userDateOfBirth, @RequestParam("userId") @Max(20) int userId
+		@RequestParam @Size(max=20) @NotBlank String name, @RequestParam("dateOfBirth") @Pattern(regexp="^([MTSH]\\d{1,2}|\\d{2,4})/?(0?[1-9]|1[0-2])/?(0?[1-9]|[1-2][0-9]|3[0-1])$") @NotBlank String dateOfBirth, @RequestParam("id") @Max(20) int id
 	) {
-		return String.format(template, userName, userDateOfBirth, userId);
+		return String.format(template, name, dateOfBirth, id);
 	}
 
 	@PostMapping("/users")
-	public ResponseEntity<Map<String, String>> userCreate(
-		@RequestBody CreateForm form
+	public ResponseEntity userCreate(
+		CreateResponse id, CreateResponse message, UriComponentsBuilder uriBuilder
 	) {
-		URI url = UriComponentsBuilder.fromUriString("http://localhost:8080").path("/users/id").build().toUri();
-		return ResponseEntity.created(url).body(Map.of("id：" + counter.incrementAndGet(), "userName successfully created"));
+		URI url = uriBuilder.path("/users/id").build().toUri();
+		return ResponseEntity.created(url).body(Map.of("id", counter.incrementAndGet(), "message", "userName successfully created"));
 	}
 
 	@PatchMapping("/users/{id}")
-	public ResponseEntity<Map<String, String>> userUpdate(@PathVariable("id") @Max(20) int id, @RequestBody UpdateForm form) {
-		return ResponseEntity.ok(Map.of("id:" + id, "userName successfully updated"));
+	public ResponseEntity userUpdate(@PathVariable("id") @Max(20) int id, @RequestBody UpdateForm form) {
+		return ResponseEntity.ok(Map.of("id", id, "message", "userName successfully updated"));
 	}
 
 	@DeleteMapping("/users/{id}")
-	public ResponseEntity<Map<String, String>> userDelete(@PathVariable("id") @Max(20) int id) {
-		return ResponseEntity.ok(Map.of("id:" + id, "userName successfully deleted"));
+	public ResponseEntity userDelete(@PathVariable("id") @Max(20) int id) {
+		return ResponseEntity.ok(Map.of("id", id, "message", "userName successfully deleted"));
 	}
 }
