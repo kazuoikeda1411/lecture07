c094f0724b4c3804562ed558073b8088dbae797b
kazuoikeda1411 <erizabesu1411@icloud.com>
2022-12-07 09:18:49 +0900

    第７講コメント修正#2(アテノーション直後のスペースを削除)

--- lecture07/src/main/java/com/raisetech/lecture07/HelloController.java
+++ lecture07/src/main/java/com/raisetech/lecture07/HelloController.java
@@ -20,14 +20,14 @@ public class HelloController {
 	private static final String template = " 登録氏名：%s 登録生年月日：%s user id：%d";
 	private final AtomicLong counter = new AtomicLong();
 
-	@GetMapping ("/users")
+	@GetMapping("/users")
 	public String greeting(
-		@RequestParam @Size (max = 20) @NotBlank String userName, @RequestParam ("userDateOfBirth") @Pattern (regexp = "^([MTSH]\\d{1,2}|\\d{2,4})/?(0?[1-9]|1[0-2])/?(0?[1-9]|[1-2][0-9]|3[0-1])$") @NotBlank String userDateOfBirth, @RequestParam ("userId") @Max (20) int userId
+		@RequestParam @Size(max=20) @NotBlank String userName, @RequestParam("userDateOfBirth") @Pattern(regexp="^([MTSH]\\d{1,2}|\\d{2,4})/?(0?[1-9]|1[0-2])/?(0?[1-9]|[1-2][0-9]|3[0-1])$") @NotBlank String userDateOfBirth, @RequestParam("userId") @Max(20) int userId
 	) {
 		return String.format(template, userName, userDateOfBirth, userId);
 	}
 
-	@PostMapping ("/users")
+	@PostMapping("/users")
 	public ResponseEntity<Map<String, String>> userCreate(
 		@RequestBody CreateForm form
 	) {
@@ -35,13 +35,13 @@ public class HelloController {
 		return ResponseEntity.created(url).body(Map.of("id：" + counter.incrementAndGet(), "userName successfully created"));
 	}
 
-	@PatchMapping ("/users/{id}")
-	public ResponseEntity<Map<String, String>> userUpdate(@PathVariable ("id") @Max (20) int id, @RequestBody UpdateForm form) {
+	@PatchMapping("/users/{id}")
+	public ResponseEntity<Map<String, String>> userUpdate(@PathVariable("id") @Max(20) int id, @RequestBody UpdateForm form) {
 		return ResponseEntity.ok(Map.of("id:" + id, "userName successfully updated"));
 	}
 
-	@DeleteMapping ("/users/{id}")
-	public ResponseEntity<Map<String, String>> userDelete(@PathVariable ("id") @Max (20) int id) {
+	@DeleteMapping("/users/{id}")
+	public ResponseEntity<Map<String, String>> userDelete(@PathVariable("id") @Max(20) int id) {
 		return ResponseEntity.ok(Map.of("id:" + id, "userName successfully deleted"));
 	}
 }
